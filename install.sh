89fdd4b15fa40caf4d84a2981651cda8ce8a00f3
John Clark <inindev@gmail.com>
2023-07-23 21:09:08 +0000

    update to latest
    Signed-off-by: John Clark <inindev@gmail.com>

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ set -e
 td="$(mktemp -d)"
 cd "$td"
 
-wget 'https://raw.githubusercontent.com/inindev/extlinux-menu/main/dtb_copy'
+wget 'https://raw.githubusercontent.com/inindev/extlinux-menu/main/dtb_cp'
 wget 'https://raw.githubusercontent.com/inindev/extlinux-menu/main/dtb_rm'
-wget 'https://raw.githubusercontent.com/inindev/extlinux-menu/main/mk_extlinux.sh'
+wget 'https://raw.githubusercontent.com/inindev/extlinux-menu/main/mk_extlinux'
 
-sudo install -vm 754 'dtb_copy' '/etc/kernel/postinst.d'
+sudo install -vm 754 'dtb_cp' '/etc/kernel/postinst.d'
 sudo install -vm 754 'dtb_rm' '/etc/kernel/postrm.d'
-sudo install -vm 754 'mk_extlinux.sh' '/boot'
-sudo ln -svf '../../../boot/mk_extlinux.sh' '/etc/kernel/postinst.d/update_extlinux'
-sudo ln -svf '../../../boot/mk_extlinux.sh' '/etc/kernel/postrm.d/update_extlinux'
+sudo install -vm 754 'mk_extlinux' '/boot'
+sudo ln -svf '../../../boot/mk_extlinux' '/etc/kernel/postinst.d/update_extlinux'
+sudo ln -svf '../../../boot/mk_extlinux' '/etc/kernel/postrm.d/update_extlinux'
 
-sudo sh '/boot/mk_extlinux.sh'
+sudo sh '/boot/mk_extlinux'
 rm -rf "$td"
 
